a0b7a9c17994f694560c5dbf0bf2ab428bd473bb
MZWANDILE0201 <majwapulam@gmail.com>
2023-05-30 13:45:32 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./data.json" assert {type: 'json'}
+
+
+
+const basicContext ={
+   };
+
+    export default {
+        plugins: [handlebars({
+           context : data 
+        })]
+    };
+
+        
+
+  
