7fff58d54be2ad971277977c56d4f45bc39c1bda
MZWANDILE0201 <majwapulam@gmail.com>
2023-05-31 14:57:18 +0200

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,27 @@
 import handlebars from 'vite-plugin-handlebars';
-import data from "./data.json" assert {type: 'json'}
+import { defineConfig } from 'vite';
+import data from './data.json';
 
+export default defineConfig({
+    base : '/responsive_webpages/',
+    build: {
+        rollupOptions: {
+            input : {
+                main: 'index.html',
+                bootstrap: 'index-bootstrap.html',
+                materialize: 'index-materialize.html',
+            }
+        },
+    },
+    plugins: [handlebars({
+      context : {
+        data
+      }
+  })]
+    
+});
 
-
-const basicContext ={
-   };
-
-    export default {
-        plugins: [handlebars({
-           context : data 
-        })]
-    };
-
+    
         
 
   
